a7b731d9ae1e73d9ac8a800fe5dc9a7ca16ae76a
nayeven allaghen <nayevenallaghen17@gmail.com>
2022-11-05 16:16:22 +0400

    show event updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_152400) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_115239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_152400) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.string "type"
     t.string "description"
     t.string "location"
     t.integer "price"
@@ -36,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_152400) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "event_type"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
