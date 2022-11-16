bb01b805f6852798d61ff6ce54b71f46a272476f
nayeven allaghen <nayevenallaghen17@gmail.com>
2022-11-16 19:08:31 +0400

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create]
     resources :reviews, only: %i[create destroy]
   end
-  
-  resources :bookings, only: %i[index edit update]
+
+  resources :bookings, only: %i[index edit update destroy]
 end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
