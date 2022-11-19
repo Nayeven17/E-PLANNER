1d31a455721e4b95a2022e57cae8279f18f5dde9
nayeven allaghen <nayevenallaghen17@gmail.com>
2022-11-19 09:48:21 +0400

    Homepage updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+      @events = Event.search_by_title_and_description(params[:query])
+    else
+      @mostrecentevent = Event.all.sort_by { |e| e[:start_date] }.first(3)
+    end
   end
 end
